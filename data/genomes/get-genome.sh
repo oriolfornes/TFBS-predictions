23cfcb451fc9856eb0ee6cbb8466422e01c7fea7
oriolfornes <oriol.fornes@gmail.com>
2021-03-24 11:24:15 -0700

    added MYC logos

--- data/genomes/get-genome.sh
+++ data/genomes/get-genome.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if ! [ -f hg38/hg38.fa.sizes ]; then
-    genomepy install -p UCSC -g ./ -r "^chr[\dXYM]{1,2}$" -f hg38
+    genomepy install -p UCSC -g ./ -r "^chr[\dXYM]{1,2}$" -m hard -f hg38
 fi
